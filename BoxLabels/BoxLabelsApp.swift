0eb21d295abbfde8c094f2684f35618c97b5cdc8
Maciej Podgórski <maciejp2001@gmail.com>
2024-05-04 12:26:19 +0200

    Initial Commit

---
+++ BoxLabels/BoxLabelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxLabelsApp.swift
+//  BoxLabels
+//
+//  Created by Maciej Podgórski on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BoxLabelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
